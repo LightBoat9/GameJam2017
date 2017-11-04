1fceebfaebed103aea00dc2a0fd54acac8a0a93b
ChippyBippers <Chitofresh1@yahoo.com>
2017-11-04 16:13:32 -0400

    act change functionality

---
+++ game/Environment/scripts/Act_manager.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+var acts = []
+var current
+
+func _ready():
+	acts.append(get_node("Act1"))
+	acts.append(get_node("Act2"))
+	current = 0
+
+func act_next():
+	acts[current].hide()
+	current+=1
+	if current>=acts.size(): current = 0
+	acts[current].show()
--- game/Environment/scripts/curtain_states.gd
+++ game/Environment/scripts/curtain_states.gd
@@ -5,12 +5,15 @@ var minY; var maxY
 var curtains
 var rate = 4
 
+var environ
+
 func _ready():
 	set_process(true)
 	set_current_state("idle")
 	curtains = get_node("curtains_back")
 	minY = -curtains.get_offset().y + 128
 	maxY = curtains.get_offset().y
+	environ = get_parent()
 
 func move(offset):
 	var pos = curtains.get_offset()
@@ -33,4 +36,6 @@ func raise_exit(): raised = true
 func lower_enter(): pass
 func lower_update():
 	if (move(rate)) : set_current_state("idle")
-func lower_exit(): raised = false
+func lower_exit(): 
+	raised = false
+	environ.act_next()
