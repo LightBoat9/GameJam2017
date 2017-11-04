e5a20c22335031292c5e5e7be5a7f294fe0a05a4
Luke <gamersloop@gmail.com>
2017-11-04 12:14:13 -0400

    on my way

--- game/Player/Scripts/PlayerStateMachine.gd
+++ game/Player/Scripts/PlayerStateMachine.gd
@@ -1,17 +1,18 @@
 extends "res://Global/StateMachine.gd"
 
-var STATE = ["idle", "walk", "jump"]
-
 func _ready():
-	set_current_state(STATE[STATE.find("idle")])
+	Player.PlayerSprites.connect("finished", self, "roll_end")
+	set_current_state("idle")
 	
 func idle_enter(): Player.PlayerSprites.set_animation("idle")
 func idle_exit(): pass
 func idle_update():
-	if (!Player.PlayerMovement.on_ground):
-		set_current_state(STATE[STATE.find("jump")])
+	if (Input.is_action_pressed("key_roll")):
+		set_current_state("roll")
+	elif (!Player.PlayerMovement.on_ground):
+		set_current_state("jump")
 	elif (_is_moving()):
-		set_current_state(STATE[STATE.find("walk")])
+		set_current_state("walk")
 	
 	Player.PlayerMovement.update()
 	Player.PlayerGraphics.update()
@@ -19,10 +20,12 @@ func idle_update():
 func walk_enter(): Player.PlayerSprites.set_animation("walk")
 func walk_exit(): pass
 func walk_update():
-	if (!Player.PlayerMovement.on_ground):
-		set_current_state(STATE[STATE.find("jump")])
+	if (Input.is_action_pressed("key_roll")):
+		set_current_state("roll")
+	elif (!Player.PlayerMovement.on_ground):
+		set_current_state("jump")
 	elif (!_is_moving()):
-		set_current_state(STATE[STATE.find("idle")])
+		set_current_state("idle")
 	
 	Player.PlayerMovement.update()
 	Player.PlayerGraphics.update()
@@ -31,12 +34,21 @@ func jump_enter(): Player.PlayerSprites.set_animation("jump")
 func jump_exit(): pass
 func jump_update():
 	if (Player.PlayerMovement.on_ground):
-		set_current_state(STATE[STATE.find("idle")])
+		set_current_state("idle")
 		
 	Player.PlayerSprites.set_frame(sign(Player.PlayerMovement.velocity.y) + 1)
 		
 	Player.PlayerMovement.update()
 	Player.PlayerGraphics.update()
+	
+func roll_enter(): 
+	Player.PlayerSprites.set_frame(0)
+	Player.PlayerSprites.set_animation("roll")
+func roll_exit(): pass
+func roll_update(): pass
+	
+func roll_end():
+	set_current_state("idle")
 	
 func _is_moving():
 	if (Input.is_action_pressed("key_right") or
