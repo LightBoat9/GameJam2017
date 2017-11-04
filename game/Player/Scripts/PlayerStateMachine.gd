ee61a8af3e56d273c8e81e87067201227f49d9eb
Luke <gamersloop@gmail.com>
2017-11-04 13:16:44 -0400

    give dem sprites boi

--- game/Player/Scripts/PlayerStateMachine.gd
+++ game/Player/Scripts/PlayerStateMachine.gd
@@ -1,16 +1,18 @@
 extends "res://Global/StateMachine.gd"
 
 func _ready():
-	Player.PlayerSprites.connect("frame_changed", self, "roll_frame_changed")
+	Player.PlayerSprites.connect("frame_changed", self, "frame_changed")
 	set_current_state("idle")
 	
 func idle_enter(): Player.PlayerSprites.set_animation("idle")
 func idle_exit(): pass
 func idle_update():
-	if (Input.is_action_pressed("key_roll")):
-		set_current_state("roll")
-	elif (!Player.PlayerMovement.on_ground):
+	if (!Player.PlayerMovement.on_ground):
 		set_current_state("jump")
+	elif (Input.is_action_pressed("key_roll")):
+		set_current_state("roll")
+	elif(Input.is_action_pressed("key_shoot")):
+		set_current_state("shoot")
 	elif (_is_moving()):
 		set_current_state("walk")
 	
@@ -20,10 +22,12 @@ func idle_update():
 func walk_enter(): Player.PlayerSprites.set_animation("walk")
 func walk_exit(): pass
 func walk_update():
-	if (Input.is_action_pressed("key_roll")):
-		set_current_state("roll")
-	elif (!Player.PlayerMovement.on_ground):
+	if (!Player.PlayerMovement.on_ground):
 		set_current_state("jump")
+	elif (Input.is_action_pressed("key_roll")):
+		set_current_state("roll")
+	elif(Input.is_action_pressed("key_shoot")):
+		set_current_state("shoot")
 	elif (!_is_moving()):
 		set_current_state("idle")
 	
@@ -35,8 +39,14 @@ func jump_exit(): pass
 func jump_update():
 	if (Player.PlayerMovement.on_ground):
 		set_current_state("idle")
-		
-	Player.PlayerSprites.set_frame(sign(Player.PlayerMovement.velocity.y) + 1)
+	
+	var y = Player.PlayerMovement.velocity.y
+	if (y < -2):
+		Player.PlayerSprites.set_frame(0)
+	elif (y > 2):
+		Player.PlayerSprites.set_frame(2)
+	else:
+		Player.PlayerSprites.set_frame(1)
 		
 	Player.PlayerMovement.update()
 	Player.PlayerGraphics.update()
@@ -48,9 +58,17 @@ func roll_exit(): pass
 func roll_update():
 	Player.PlayerMovement.roll_update()
 		
-func roll_frame_changed():
-	if (Player.PlayerSprites.get_frame() == 0):
-		set_current_state("idle")
+func shoot_enter():
+	Player.PlayerSprites.set_frame(0)
+	Player.PlayerSprites.set_animation("shoot")
+func shoot_exit(): pass
+func shoot_update(): pass
+	
+func frame_changed():
+	var a = Player.PlayerSprites.get_animation()
+	if (a == "roll" || a == "shoot"):
+		if (Player.PlayerSprites.get_frame() == 0):
+			set_current_state("idle")
 	
 func _is_moving():
 	if (Input.is_action_pressed("key_right") or
